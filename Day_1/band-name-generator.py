84824ab9d19c2752392241e75f72070f8d8935ea
Arun Varma <arunvarma25683@gmail.com>
2026-01-06 23:00:07 +0530

    Day 1 Project Completed

---
+++ Day_1/band-name-generator.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator.')
+user_city = input('What is the name of the city you grew up in?\n')
+pet_name = input('What is your pet name?\n')
+print('Your band name could be ' + user_city + ' ' + pet_name)
